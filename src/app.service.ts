5c3fa7ee2d4ceaeaa1d5d59af8b1094c541ef21b
vgvishesh <vgvishesh2008@gmail.com>
2023-07-19 14:37:38 +0530

    milestone 1, prompting basic questions

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { OpenAI } from 'langchain/llms/openai';
+import { configuration } from './config/config';
 
 @Injectable()
 export class AppService {
+  protected model: OpenAI;
+  constructor() {
+    this.model = new OpenAI({
+      openAIApiKey: configuration().OPEN_API_KEY,
+      temperature: 0.9,
+    });
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
 
-  doingMagic(): string {
-    return 'this is it!';
+  async doingMagic(): Promise<string> {
+    const res = await this.model.call(
+      'what would be a good company name for a compnay that makes colorful tin cans',
+    );
+
+    return res;
   }
 }
---
+++ src/config/config.ts
@@ -0,0 +1,4 @@
+import 'dotenv/config';
+export const configuration = () => ({
+  OPEN_API_KEY: process.env.OPEN_API_KEY,
+});
